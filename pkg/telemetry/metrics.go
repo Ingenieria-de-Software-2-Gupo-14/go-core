5ddf8325e2a75d499ecf770869e705651cd9ee6f
Martin Pata <martin.pata@mercadolibre.com>
2025-05-13 01:55:49 -0300

    feat: remove err

--- pkg/telemetry/metrics.go
+++ pkg/telemetry/metrics.go
@@ -7,20 +7,20 @@ import (
 
 // Count tracks how many times something happened per second.
 func Count(ctx context.Context, name string, value int64, tags []string) {
-	FromContext(ctx).Count(name, value, tags)
+	FromContext(ctx).Count(ctx, name, value, tags)
 }
 
 // Decr is just Count of -1.
 func Decr(ctx context.Context, name string, tags []string) {
-	FromContext(ctx).Decr(name, tags)
+	FromContext(ctx).Decr(ctx, name, tags)
 }
 
 // Incr is just Count of 1.
 func Incr(ctx context.Context, name string, tags []string) {
-	FromContext(ctx).Incr(name, tags)
+	FromContext(ctx).Incr(ctx, name, tags)
 }
 
 // Timing sends timing information, it is an alias for TimeInMilliseconds.
 func Timing(ctx context.Context, name string, value time.Duration, tags []string) {
-	FromContext(ctx).Timing(name, value, tags)
+	FromContext(ctx).Timing(ctx, name, value, tags)
 }
