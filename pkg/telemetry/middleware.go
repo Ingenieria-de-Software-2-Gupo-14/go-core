f8d50de5e700436e71c70c9454feeb7d91a59bc0
Martin Pata <martin.pata@mercadolibre.com>
2025-05-15 01:27:51 -0300

    feat: change middleware

--- pkg/telemetry/middleware.go
+++ pkg/telemetry/middleware.go
@@ -1,13 +1,27 @@
 package telemetry
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
-func AddTracer(tracer Client) gin.HandlerFunc {
+func MetricsMiddleware(metricsClient Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx := Context(c.Request.Context(), tracer)
+		ctx := Context(c.Request.Context(), metricsClient)
 		c.Request = c.Request.WithContext(ctx)
+
+		startTime := time.Now()
 		c.Next()
+
+		tags := []string{
+			"method:" + c.Request.Method,
+			"path:" + c.FullPath(),
+			"status:" + strconv.Itoa(c.Writer.Status()),
+		}
+
+		metricsClient.Incr(ctx, "traffic.request", tags...)
+		metricsClient.Timing(ctx, "traffic.response.time", time.Since(startTime), tags...)
 	}
 }
